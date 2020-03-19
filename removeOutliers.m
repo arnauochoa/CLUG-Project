bd19ea5afd76021cea31083dc078d65b63b176bc
arnauochoa <arnau.ochoa@gmail.com>
2020-03-19 01:22:44 +0100

    fitting 3d done, some plots shown

--- removeOutliers.m
+++ removeOutliers.m
@@ -3,6 +3,7 @@ function DATA = removeOutliers(DATA, CONFIG)
     keepPoints  =   bitand( DATA.Y < CONFIG.DATA.Y_UPPER_THRESHOLD,     ...
                             DATA.Y > CONFIG.DATA.Y_LOWER_THRESHOLD      ...
                             );
-    DATA.X      =   DATA.X(keepPoints);
+    
+    DATA.X      =   DATA.X(keepPoints, :);
     DATA.Y      =   DATA.Y(keepPoints);
 end
